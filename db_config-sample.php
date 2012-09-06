7ef9b72f4bbebf3a9c2d2359c3d4eb1537072d86
Jo Giraerts <whachoe@gmail.com>
2012-09-07 00:52:58 +0200

    changed unix-socket to host

--- db_config-sample.php
+++ db_config-sample.php
@@ -2,7 +2,7 @@
   // Mysql DB
   define('MYSQL_USER', 'your_phpbb_user');
   define('MYSQL_PW', 'your_phpbb_password');
-  define('MYSQL_DSN', 'mysql:unix_socket=/tmp/mysql.sock;dbname=your_phpbb_databasename');
+  define('MYSQL_DSN', 'mysql:host=localhost;dbname=your_phpbb_databasename');
   
   $options = array(
       PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
