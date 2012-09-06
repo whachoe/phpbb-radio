741067e39645679609468b50a32d401337898af7
Jo Giraerts <whachoe@gmail.com>
2012-09-06 22:15:45 +0200

    Soundcloud api key + README

--- config.php
+++ config.php
@@ -5,6 +5,11 @@
   define('MONGO_DB', "breakzradio");
   define('MONGO_COLLECTION', "tracks");
   
+  // Soundcloud API Key: This to be able to play soundcloud-links
+  // If your forum doesn't have any links to soundcloud-urls, you can ignore this
+  // 
+  // Get one at http://soundcloud.com/you/apps
+  define('SOUNDCLOUD_API_KEY', 'YOUR_SOUNDCLOUD_API_KEY');
   
   // Various constants used in the website
   define("CONTACTEMAIL", "cjpa@breakzforum.be");
